d073d9c8c9e7dafe38d01494d4fd29084735bb15
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-18 23:14:45 +0100

    Created topic publisher interface

---
+++ src/Smiosoft.PASS.ServiceBuss/Topic/ITopicPublisher.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Smiosoft.PASS.Publisher;
+
+namespace Smiosoft.PASS.ServiceBuss.Topic
+{
+	public interface ITopicPublisher : IBasePublisher
+	{ }
+
+	public interface ITopicPublisher<TMessage> : ITopicPublisher, IPublisher<ITopicClient, TMessage>
+		where TMessage : class
+	{ }
+}
