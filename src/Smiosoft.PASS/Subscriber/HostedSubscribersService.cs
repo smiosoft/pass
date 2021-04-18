9999e44b64be8c1e64ae10d914f2105914c42f5a
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-18 23:08:39 +0100

    Created subscribers service as a background service

---
+++ src/Smiosoft.PASS/Subscriber/HostedSubscribersService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Smiosoft.PASS.Subscriber
+{
+	internal class HostedSubscribersService : BackgroundService
+	{
+		private readonly IServiceProvider _provider;
+
+		public HostedSubscribersService(IServiceProvider provider)
+		{
+			_provider = provider;
+		}
+
+		protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			var subscribers = _provider.GetServices<IBaseSubscriber>();
+			foreach (var subscriber in subscribers)
+			{
+				subscriber.Register();
+			}
+
+			return Task.CompletedTask;
+		}
+	}
+}
