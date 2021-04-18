cc35a873716876ea69585636dbc93183b69f81bb
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-18 23:05:07 +0100

    Created publisher interface

---
+++ src/Smiosoft.PASS/Publisher/IPublisher.cs
@@ -0,0 +1,7 @@
+﻿namespace Smiosoft.PASS.Publisher
+{
+	public interface IPublisher<TClient, TMessage> : IBasePublisher, IClient<TClient>, IMessagePublisher<TMessage>
+		where TClient : class
+		where TMessage : class
+	{ }
+}
