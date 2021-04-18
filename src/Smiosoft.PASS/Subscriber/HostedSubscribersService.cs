658d4dae2e5e4e293494ea405487fb0a622ad74d
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-18 23:08:51 +0100

    Added missing using

--- src/Smiosoft.PASS/Subscriber/HostedSubscribersService.cs
+++ src/Smiosoft.PASS/Subscriber/HostedSubscribersService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
