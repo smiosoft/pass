093428522c4c4ed11f36f19c2e812d50763c16a9
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-18 23:07:43 +0100

    Created publishers service

---
+++ src/Smiosoft.PASS/Publisher/IPublishersService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Smiosoft.PASS.Publisher
+{
+	public interface IPublishersService
+	{
+		Task PublishAsync<TMessage>(TMessage message) where TMessage : class;
+		Task<bool> TryPublishAsync<TMessage>(TMessage message) where TMessage : class;
+	}
+}
---
+++ src/Smiosoft.PASS/Publisher/PublisherNotRegisteredException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Smiosoft.PASS.Publisher
+{
+	public class PublisherNotRegisteredException : Exception
+	{
+		public PublisherNotRegisteredException(string message) : base(message)
+		{ }
+	}
+}
