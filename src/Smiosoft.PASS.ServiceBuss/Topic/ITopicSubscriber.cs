e739b17dac26cce7afe836eaabc142c4a9600a5b
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-18 23:15:34 +0100

    Created topic subscriber interface

---
+++ src/Smiosoft.PASS.ServiceBuss/Topic/ITopicSubscriber.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Smiosoft.PASS.Subscriber;
+
+namespace Smiosoft.PASS.ServiceBuss.Topic
+{
+	public interface ITopicSubscriber : IBaseSubscriber
+	{ }
+
+	public interface ITopicSubscriber<TMessage> : ITopicSubscriber, ISubscriber<ISubscriptionClient, TMessage>
+		where TMessage : class
+	{ }
+}
