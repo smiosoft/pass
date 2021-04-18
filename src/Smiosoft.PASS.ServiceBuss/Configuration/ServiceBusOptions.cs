6a95df0daba813b95115f02f4554166dd081ee26
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-18 23:12:43 +0100

    Created service bus DI extensions with configuration

---
+++ src/Smiosoft.PASS.ServiceBuss/Configuration/ServiceBusOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Smiosoft.PASS.ServiceBuss.Configuration
+{
+	public class ServiceBusOptions
+	{
+		private readonly IServiceCollection _services;
+
+		public ServiceBusOptions(IServiceCollection services)
+		{
+			_services = services;
+		}
+	}
+}
---
+++ src/Smiosoft.PASS.ServiceBuss/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Smiosoft.PASS.ServiceBuss.Configuration;
+using System;
+
+namespace Smiosoft.PASS.ServiceBuss
+{
+	public static class StartupInjection
+	{
+		public static IServiceCollection AddPassServiceBus(this IServiceCollection services, Action<ServiceBusOptions> options)
+		{
+			options(new ServiceBusOptions(services));
+
+			services
+				.AddPassPublishersService()
+				.AddPassSubscribersService();
+
+			return services;
+		}
+	}
+}
