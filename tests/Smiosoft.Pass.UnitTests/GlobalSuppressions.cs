1a51f1cf977dec16b5d3cefaf8f52896f21fe35e
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-21 22:16:12 +0100

    Created global suppressions file for tests project

---
+++ tests/Smiosoft.Pass.UnitTests/GlobalSuppressions.cs
@@ -0,0 +1,3 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1806:Do not ignore method results", Justification = "Tests can create objects that are never used", Scope = "namespaceanddescendants", Target = "~N:Smiosoft.Pass.UnitTests")]
