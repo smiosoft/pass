5f674226b84359bc8d6a46f482a863b09b36fe9d
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-21 21:59:07 +0100

    Created empty publishers service unit test class

---
+++ tests/Smiosoft.Pass.UnitTests/Publisher/PublishersServiceTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Moq;
+using Smiosoft.PASS.Publisher;
+
+namespace Smiosoft.Pass.UnitTests.Publisher
+{
+	public class PublishersServiceTests
+	{
+		private readonly Mock<IServiceProvider> _mockServiceProvider;
+		private readonly PublishersService _sut;
+
+		public PublishersServiceTests()
+		{
+			_mockServiceProvider = new Mock<IServiceProvider>();
+
+			_sut = new PublishersService(_mockServiceProvider.Object);
+		}
+	}
+}
