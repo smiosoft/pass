7525031f921827cd3f71e02631ed93e4c63b8be8
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-04-22 23:02:36 +0100

    Created service bus options tests

---
+++ tests/Smiosoft.PASS.ServiceBus.UnitTests/TestHelpers/Mocks/Factories/MockServiceDescriptorFactory.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Smiosoft.PASS.ServiceBus.UnitTests.TestHelpers.Mocks.Factories
+{
+	public static class MockServiceDescriptorFactory
+	{
+		public static ServiceDescriptor CreateIt(ServiceLifetime lifetime, Type service, Type implementation)
+		{
+			return It.Is<ServiceDescriptor>(_ =>
+				_.Lifetime == lifetime
+				&& _.ServiceType == service
+				&& _.ImplementationType == implementation);
+		}
+	}
+}
