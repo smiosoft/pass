fe219386fa7dc30a732c27423f584c6a56118064
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2022-05-01 19:23:18 +0100

    Added exception handler for publishers

--- src/Smiosoft.PASS/Publisher/IMessagePublisher.cs
+++ src/Smiosoft.PASS/Publisher/IMessagePublisher.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Threading.Tasks;
 
 namespace Smiosoft.PASS.Publisher
 {
 	public interface IMessagePublisher<TMessage>
 	{
+		Task OnExceptionAsync(Exception exception);
 		Task PublishAsync(TMessage message);
 	}
 }
