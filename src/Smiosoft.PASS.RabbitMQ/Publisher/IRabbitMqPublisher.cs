2c09e8cf86e5afbbd18eb646036440f208453fa3
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2022-05-03 21:26:05 +0100

    Emptied rabbitmq library

--- src/Smiosoft.PASS.RabbitMQ/Publisher/IRabbitMqPublisher.cs
+++
@@ -1,9 +0,0 @@
-namespace Smiosoft.PASS.RabbitMQ.Publisher
-{
-	public interface IRabbitMqPublisher : IBasePublisher
-	{ }
-
-	public interface IRabbitMqPublisher<TMessage> : IRabbitMqPublisher, IPublisher<TMessage>
-		where TMessage : class
-	{ }
-}
