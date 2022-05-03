de90cf869ce59ba56ad070096c6f2c447e4d5b3b
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2022-05-03 21:10:15 +0100

    Created interface defining a handler for publishing

---
+++ src/Smiosoft.PASS/Publisher/IPublishingHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Smiosoft.PASS.Publisher
+{
+	/// <summary>
+	/// Defines a handler for publishing a payload
+	/// </summary>
+	/// <typeparam name="TPayload">The type of payload being handled</typeparam>
+	public interface IPublishingHandler<in TPayload>
+		where TPayload : IPayload
+	{
+		/// <summary>
+		/// Handles publishing a payload
+		/// </summary>
+		/// <param name="payload">The payload</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>An awaitable task</returns>
+		Task HandleAsync(TPayload payload, CancellationToken cancellationToken);
+	}
+}
