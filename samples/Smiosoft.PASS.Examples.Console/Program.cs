ccae9bdda8bad14ec2720a7a98bf0625e2274938
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2022-05-03 20:36:14 +0100

    Added DI to the sample project

--- samples/Smiosoft.PASS.Examples.Console/Program.cs
+++ samples/Smiosoft.PASS.Examples.Console/Program.cs
@@ -1,2 +1,5 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+
