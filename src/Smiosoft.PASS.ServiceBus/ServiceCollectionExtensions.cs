ad9e802137efbdc178df07d8e08db0740999b8ff
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2022-05-04 14:56:50 +0100

    Added registration config to restrict usage to service bus

---
+++ src/Smiosoft.PASS.ServiceBus/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+namespace Smiosoft.PASS.ServiceBus
+{
+	/// <summary>
+	/// Extensions to register everything PASS for Service Bus
+	/// </summary>
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Restricts registration to only Service Bus related PASS types
+		/// </summary>
+		/// <param name="source">PASS service configuration</param>
+		/// <returns>PASS service configuration</returns>
+		public static PassServiceConfiguration WithServiceBus(this PassServiceConfiguration source)
+		{
+			source.WithEvaluator((type) => typeof(IServiceBus).IsAssignableFrom(type));
+			return source;
+		}
+	}
+}
